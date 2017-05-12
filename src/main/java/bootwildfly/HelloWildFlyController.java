5055ac1d6a3bc52302a2c0c775280338e2b60d0c
350401 <pavan.rankireddy@gmail.com>
2017-05-12 20:37:17 +0530

    updated

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -9,6 +9,6 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("User : " + System.getenv("OPENSHIFT_MYSQL_DB_USERNAME") +" ,Password : "+ System.getenv("OPENSHIFT_MYSQL_DB_URL"));
+        return (" MYSQL Database added to classpath");
     }
 }
